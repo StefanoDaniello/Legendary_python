f8d51bea8fd464e724a375d6ea238577ee54e839
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-22 23:13:02 +0200

    add variables

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello Word")
+# citta = ["Roma", "Milano", "Napoli", "Palermo"]
+
+# x,y,z,w = citta
+# print(x,y,z,w)
+
+
+# x= 10
+# y=2
+# z= x+y+x+x
+
+# print(x,y,z)
+
+
+
+
+# Tipi di Dati  
+
+# x = 1.0
+# print(type(x))
+
+# x= "d"
+# print(type(x))
+
+# Casting
+
+x = str(1.9)
+print(x)
